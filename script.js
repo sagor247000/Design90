6212421c6ed79f513db67bf39232f284457e1843
sagor247000 <sagor247000000@gmail.com>
2026-04-16 23:17:41 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// বাটনে ক্লিক করলে একটি এলার্ট দেখাবে
+document.getElementById('magicBtn').addEventListener('click', function() {
+    alert('অভিনন্দন! 🎉 আপনার ওয়েবসাইটটি Netlify তে সফলভাবে কাজ করছে। আপনি এখন একজন ওয়েব ডেভেলপার হওয়ার পথে!');
+});
